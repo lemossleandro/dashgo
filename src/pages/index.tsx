d8d7601acd9f0689c29fb3b17a16973d99520e7a
Leandro Lemos <lemos.leandro@outlook.com>
2021-12-10 15:03:03 -0300

    Comit Inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@chakra-ui/core';
+import { Flex, Input } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center">
+
+
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Input name="email" placeholder="Email" />
+        <Input name="password" placeholder="Password" />
+        <Button type='submit' mt="6">Entrar</Button>
+      </Flex>
+    </Flex >
+
+  )
+}
